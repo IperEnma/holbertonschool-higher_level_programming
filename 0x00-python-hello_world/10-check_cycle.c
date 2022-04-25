9b0dde868b51733339f4ab1e256dada868a0abfa
IperEnma <enmanuelhernandez1843@gmail.com>
2022-04-25 08:41:57 -0700

    python first day

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * check_cycle - check if the list is circular
+ * list - pointer to list
+ * Return: 0 no cycle or 1 yes cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *aux = list;
+
+	while (aux)
+	{
+		aux = aux->next;
+		if (aux == list)
+			return (1);
+	}
+
+	return (0);
+}
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+for x in range(0, 3): print(str, end = '')
+print("")
+print(str[0:9])
