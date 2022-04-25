06b69dbf3034200fbec9ed311cd665fce9fc1623
IperEnma <enmanuelhernandez1843@gmail.com>
2022-04-25 08:57:42 -0700

    pip

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
-for x in range(0, 3): print(str, end = '')
-print("")
-print(str[0:9])
+for x in range(0, 3):
+    print(str, end='')
+print("\n{}".format(str[0:9]))
