6038dfab96c7dfff550e284152001e756fd10b97
IperEnma <enmanuelhernandez1843@gmail.com>
2022-04-26 09:52:39 -0700

    day 2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for x in range (0, 100):
-    print("{}".format(str(x).zfill(2)))
+for x in range(0, 100):
+    print("{}".format(str(x).zfill(2)), end="")
+    if x != 99:
+        print(f", ", end="")
+    if x == 99:
+        print("")
