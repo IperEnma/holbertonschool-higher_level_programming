9dd422bac8a47c4ee6607711fa13642c254d75c5
IperEnma <enmanuelhernandez1843@gmail.com>
2022-04-26 11:33:17 -0700

    funciton

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,16 @@
+#!/bin/bash
+def islower(c):
+    asc = ord(c)
+    if asc > 96 and asc < 123:
+        return True
+    else:
+        return False
+
+
+def uppercase(str):
+    for x in str:
+        if islower(x):
+            y = ord(x) - 32
+            x = chr(y)
+        print(f"{x}", end="")
+    print("")
