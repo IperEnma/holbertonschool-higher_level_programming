9b44da253e673ee863ceb65ec57cba57053f2301
IperEnma <enmanuelhernandez1843@gmail.com>
2022-04-28 08:46:20 -0700

    modules, impor

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
-t = len(sys.argv)
-i = 1
-resultado = 0
-if (t == 2):
-    print(f"{sys.argv[1]}")
-elif (t > 1):
-    for x in (1, t):
-        resultado += int(sys.argv[i])
-    print(f"{resultado}")
-else:
-    print("0")
+if __name__ == "__main__":
+    t = len(sys.argv)
+    i = 1
+    resultado = 0
+    if (t == 2):
+        print(f"{sys.argv[1]}")
+    elif (t > 1):
+        for x in (1, t):
+            resultado += int(sys.argv[i])
+        print(f"{resultado}")
+    else:
+        print("0")
