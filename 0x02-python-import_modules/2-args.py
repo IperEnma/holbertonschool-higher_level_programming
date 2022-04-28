fc8b7108d3da8ba7d23cf81f700d2736b76ee32e
IperEnma <enmanuelhernandez1843@gmail.com>
2022-04-28 08:42:10 -0700

    modules, impor

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if (t == 1):
         print("0 arguments.")
     else:
-        print(f"{t - 1} argument:")
+        print(f"{t - 1} arguments:")
         for x in range(1, t):
             print(f"{i}: {sys.argv[i]}")
             i = i + 1
