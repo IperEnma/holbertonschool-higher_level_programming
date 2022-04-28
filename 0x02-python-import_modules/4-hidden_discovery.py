85cde9df01789de88a9300f8a0d6359699394668
IperEnma <enmanuelhernandez1843@gmail.com>
2022-04-28 09:59:38 -0700

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
-save = dir(hidden_4)
-print(f"{save[9]}")
-print(f"{save[10]}")
-print(f"{save[11]}")
+if __name__ == "__main__":
+    save = dir(hidden_4)
+    print(f"{save[9]}")
+    print(f"{save[10]}")
+    print(f"{save[11]}")
