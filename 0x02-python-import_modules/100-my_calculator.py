15c0da08c4dd49b5ba0e9b4bd3c4a39ab9d383fb
IperEnma <enmanuelhernandez1843@gmail.com>
2022-04-28 10:47:34 -0700

    advanced

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,16 +4,21 @@ import sys
 if __name__ == "__main__":
     t = len(sys.argv)
     if (t != 4):
-    	print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    	exit(1)
-    elif (sys.argv[2] != '+' and sys.argv[2] != '-' and sys.argv[2] != '*' and sys.argv[2] != '/'):
-    	print("Unknown operator. Available operators: +, -, * and /")
-    	exit(1)
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif (sys.argv[2] != '+' and sys.argv[2] != '-'
+            and sys.argv[2] != '*' and sys.argv[2] != '/'):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
     elif (sys.argv[2] == '+'):
-        print(f"{int(sys.argv[1])} + {int(sys.argv[3])} = {add(int(sys.argv[1]), int(sys.argv[3]))}")
+        print(f"{int(sys.argv[1])} + {int(sys.argv[3])} = \
+{add(int(sys.argv[1]), int(sys.argv[3]))}")
     elif (sys.argv[2] == '-'):
-        print(f"{int(sys.argv[1])} - {int(sys.argv[3])} = {sub(int(sys.argv[1]), int(sys.argv[3]))}")
+        print(f"{int(sys.argv[1])} - {int(sys.argv[3])} = \
+{sub(int(sys.argv[1]), int(sys.argv[3]))}")
     elif (sys.argv[2] == '*'):
-        print(f"{int(sys.argv[1])} * {int(sys.argv[3])} = {mul(int(sys.argv[1]), int(sys.argv[3]))}")
+        print(f"{int(sys.argv[1])} * {int(sys.argv[3])} = \
+{mul(int(sys.argv[1]), int(sys.argv[3]))}")
     elif (sys.argv[2] == '/'):
-        print(f"{int(sys.argv[1])} / {int(sys.argv[3])} = {div(int(sys.argv[1]), int(sys.argv[3]))}")
+        print(f"{int(sys.argv[1])} / {int(sys.argv[3])} = \
+{div(int(sys.argv[1]), int(sys.argv[3]))}")
