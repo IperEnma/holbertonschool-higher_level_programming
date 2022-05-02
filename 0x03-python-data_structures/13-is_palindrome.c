683cac4d8f94e81d8d9fca40e54a5dea378905d8
IperEnma <enmanuelhernandez1843@gmail.com>
2022-05-02 15:22:47 -0700

    list

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+ *
+ *
+ */
+void addnode(listint_t **head, int n)
+{
+	listint_t *new = NULL;
+
+	new = malloc(sizeof(listint_t));
+	if (new)
+	{
+		new->n = n;
+		new->next = *head;
+		*head = new;
+	}
+}
+/**
+ *
+ *
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *new = NULL;
+	listint_t *aux = *head;
+	listint_t *newaux = NULL;
+	if (*head == NULL)
+		return (0);
+	while(aux)
+	{
+		addnode(&new, aux->n);
+		aux = aux->next;
+	}
+	aux = *head;
+	newaux = new;
+	while (aux && newaux)
+	{
+		if (aux->n != newaux->n)
+			return (0);
+		aux = aux->next;
+		newaux = newaux->next;
+	}
+	return (1);
+}
