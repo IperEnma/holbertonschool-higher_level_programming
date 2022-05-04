1d80aed166dea712232f6992a249a809cd291aef
IperEnma <enmanuelhernandez1843@gmail.com>
2022-05-04 15:42:49 -0700

    average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    new = []
+    result = 0
+    div = 0
+    for x in my_list:
+        temp = x[0] * x[1]
+        new.append(temp)
+        div = div + x[1]
+    for x in new:
+        result = result + x
+    result = result / div
+    return result
