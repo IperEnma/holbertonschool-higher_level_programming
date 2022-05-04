17330ff49be134f002b02ef66200eb5f2c9a8006
IperEnma <enmanuelhernandez1843@gmail.com>
2022-05-04 08:37:17 -0700

    common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    
+    new = []
+    for x in set_1:
+        for y in set_2:
+            if x == y:
+                new.append(y)
+    return new
