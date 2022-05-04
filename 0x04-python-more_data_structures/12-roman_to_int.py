11364ed4944143669ef0abd5fad44a7a429cbc5f
IperEnma <enmanuelhernandez1843@gmail.com>
2022-05-04 13:55:18 -0700

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (not(isinstance(roman_string, str)) or roman_string == None):
+    if (not(isinstance(roman_string, str)) or roman_string is None):
         return 0
-    roman_dictionary = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'M': 1000}
+    roman_dictionary = {
+            'I': 1,
+            'IV': 4,
+            'V': 5,
+            'IX': 9,
+            'X': 10,
+            'XL': 40,
+            'L': 50,
+            'XC': 90,
+            'C': 100,
+            'CD': 400,
+            'D': 500,
+            'M': 1000}
     result = 0
     save = 0
     for x in roman_string:
