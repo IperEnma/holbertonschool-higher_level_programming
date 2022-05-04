bf677ddf0c88d6f3386d3f442f43250b206aec4e
IperEnma <enmanuelhernandez1843@gmail.com>
2022-05-04 14:58:19 -0700

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (not(roman_string) or not(isinstance(roman_string, str))):
+    if (not(len(roman_string)) or not(isinstance(roman_string, str))):
         return 0
     roman_dictionary = {
             'I': 1,
