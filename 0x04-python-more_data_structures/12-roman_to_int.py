a5b543209c953c48b55557c52b37f40ef84722fd
IperEnma <enmanuelhernandez1843@gmail.com>
2022-05-04 13:29:19 -0700

    roman

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_dictionary = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'M': 1000}
+    result = 0
+    save = 0
+    for x in roman_string:
+        for key, value in roman_dictionary.items():
+            if x == key:
+                if save < value:
+                    result = value - result
+                else:
+                    result = result + value
+                save = value
+    return result
