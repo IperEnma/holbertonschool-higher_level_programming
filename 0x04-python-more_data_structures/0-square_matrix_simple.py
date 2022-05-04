9795a8428dd881f22ad8bace502a0b839f62af0d
IperEnma <enmanuelhernandez1843@gmail.com>
2022-05-04 08:32:17 -0700

    power matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-    count = 0
-    for x in matrix:
-        count2 = 0
-        for y in x:
-            new[count][count2] = y * y
-            count2 = count2 + 1
-        count = count + 1
+    new = []
+    value = 0
+    for x in range(len(matrix)):
+        new.append([])
+        for y in range(len(matrix[x])):
+            value = matrix[x][y] ** 2
+            new[x].append(value)
     return new
