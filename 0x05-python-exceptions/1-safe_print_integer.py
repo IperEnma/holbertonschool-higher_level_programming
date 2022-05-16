3daf18e4e6674a72bf90c17e42528e27ca32ad59
IperENma <enmanuelhernandez1843@gmail.com>
2022-05-16 08:45:39 -0700

    exception

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        validator = vale / 2
+        print(value)
+        return(True)
+    except:
+        return(False)
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new = []
+    for y in range(0, list_length):
+        try:
+            new.append(my_list_1[y] / my_list_2[y])
+        except TypeError:
+            new.append(0)
+            print("wrong type")
+        except ZeroDivisionError:
+            new.append(0)
+            print("division by 0")
+        except IndexError:
+            new.append(0)
+            print("out of range")
+        except Exception as e:
+            print("Ha ocurrido un error no previsto", type(e).__name__ )
+    return(new)
+
+
