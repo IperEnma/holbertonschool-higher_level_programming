d1d3f600632fc05ffbac4fbe1bbc019ed7abd63b
IperENma <enmanuelhernandez1843@gmail.com>
2022-05-16 08:52:37 -0700

    fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         validator = vale / 2
-        print(value)
+        print("{}".format(value))
         return(True)
     except:
         return(False)
