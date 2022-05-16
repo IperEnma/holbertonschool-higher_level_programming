c23af45bdb765a358ffbd00519b63064ade0bfa6
IperENma <enmanuelhernandez1843@gmail.com>
2022-05-16 08:47:24 -0700

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for y in range(0, x):
         try:
-            print(my_list[y], end = "")
+            print("{}".format(my_list[y]), end = "")
             count = count + 1;
         except:
             break
