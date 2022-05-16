d1169313b5c2cbb3367a7749e162337f2374aa6b
IperENma <enmanuelhernandez1843@gmail.com>
2022-05-16 13:15:04 -0700

    exceptiuon

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,8 +7,8 @@ def safe_function(fct, *args):
         result = fct(*args)
         return result
     except ZeroDivisionError:
-        print("Exception: division by zero")
+        print("Exception: division by zero", file=sys.stderr)
         return None
     except IndexError:
-        print("Exception: list index out of range")
+        print("Exception: list index out of range", file=sys.stderr)
         return None
