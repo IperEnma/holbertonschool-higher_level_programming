70cdbcc2c1c095e75df12db552c4d43f814efac2
IperENma <enmanuelhernandez1843@gmail.com>
2022-05-17 14:50:49 -0700

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,8 +6,8 @@ class Square:
     """define classs"""
     def __init__(self, size=0, position=(0, 0)):
         """initialize class"""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         """calculating area"""
