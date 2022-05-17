c63685de4415377c135be726c1d65669dad6ca5a
IperENma <enmanuelhernandez1843@gmail.com>
2022-05-17 12:36:10 -0700

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,8 +44,9 @@ class Square():
     def my_print(self):
         """print square"""
         if self.__size != 0:
-            for x in range(0, self.__position[1]):
-                print()
+            if self.__position[1] > 0:
+                for x in range(0, self.__position[1]):
+                        print()
             for x in range(0, self.__size):
                 for i in range(0, self.__position[0]):
                     print(" ", end="")
