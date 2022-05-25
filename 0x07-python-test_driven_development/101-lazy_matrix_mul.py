663e306a907f91ec2292888a783fa66eee277d96
IperEnma <enmanuelhernandez1843@gmail.com>
2022-05-25 20:04:04 -0300

    lazy

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module with function"""
+
+import numpy
+
+def lazy_matrix_mul(m_a, m_b):
+    """numpy multuply"""
+
+    resultado = numpy.matmul(m_a, m_b)
+    return resultado
