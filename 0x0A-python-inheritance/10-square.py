24bdaf95f2b93c0d08f3b04008709aedd6528e24
IperENma <enmanuelhernandez1843@gmail.com>
2022-05-30 19:33:13 -0300

    test

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,9 +9,12 @@ class Square(base):
 
     def __init__(self, size):
         """define class"""
-        super().__init__(size, size)
+        self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
         """return area square"""
         return self.__size ** 2
+    def __str__(self):
+        """Square description"""
+        return("[Rectangle] {}/{}".format(self.__size, self.__size))
