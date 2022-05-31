78bdd0426a40083a6e99d3205c38abf7be2c4224
IperENma <enmanuelhernandez1843@gmail.com>
2022-05-31 17:01:57 -0300

    input/output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,5 +9,5 @@ def save_to_json_file(my_obj, filename):
 
     json_string = json.dumps(my_obj)
 
-    with open(filename, 'w') as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         f.write(json_string)
