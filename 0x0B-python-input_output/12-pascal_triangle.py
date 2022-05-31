0b636007e11d1f164f7d5b4ee54689908491fe10
IperENma <enmanuelhernandez1843@gmail.com>
2022-05-31 19:33:40 -0300

    fix

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,4 +5,19 @@
 def pascal_triangle(n):
     """function triangle pascal"""
 
-
+    lists = []
+    for x in range(n):
+        temp_list = []
+        num1 = -1
+        num2 = 0
+        temp_list = []
+        for m in range(x + 1):
+            if num1 == -1 or num2 == len(aux):
+                temp_list.append(1)
+            else:
+                temp_list += [aux[num1] + aux[num2]]
+            num1 += 1
+            num2 += 1
+        lists.append(temp_list)
+        aux = temp_list[:]
+    return lists
