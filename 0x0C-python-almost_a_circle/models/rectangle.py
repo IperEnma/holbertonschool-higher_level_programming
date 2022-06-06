04e31cba48a98301bef88cf6908ea1b4ff216f49
IperEnma <enmanuelhernandez1843@gmail.com>
2022-06-06 16:28:35 -0300

    fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -127,5 +127,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """the dictionary representation of a Rectangle"""
-        return {'id': self.id, 'width': self.width, 'height':
-                self.height, 'x': self.x, 'y': self.y}
+        return {"id": self.id, "width": self.width, "height":
+                self.height, "x": self.x, "y": self.y}
