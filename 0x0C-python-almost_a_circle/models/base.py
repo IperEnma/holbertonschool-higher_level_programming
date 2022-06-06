38fd6aec003575d7a3a76f81a0762779509eabbb
IperEnma <enmanuelhernandez1843@gmail.com>
2022-06-06 16:49:58 -0300

    strr

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -47,7 +47,10 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
-        new = cls(1, 2)
+        if cls.__name__ == "Rectangle":
+            new = cls(1, 1)
+        if cls.__name__ == "Square":
+            new = cls(1)
         if (new):
             new.update(**dictionary)
         return(new)
