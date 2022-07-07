d994c68d647787b4ece7c369a302119ff18cdf00
Enmanuel <enmanuelhernandez1843@gmail.com>
2022-07-06 18:15:52 -0700

    last

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,6 @@
+-- lists all shows, and all genres linked 
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
-JOIN tv_gen res ON tv_genres.id=tv_show_genres.genre_id
-ORDER BY tv_shows.title ASC, tv_genres.name;
+LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
