b59c24ae0ffc000ad5be0ee568237b1f66035ae8
Enmanuel <enmanuelhernandez1843@gmail.com>
2022-07-06 18:01:47 -0700

    order shows and genree

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
+JOIN tv_gen res ON tv_genres.id=tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_genres.name;
