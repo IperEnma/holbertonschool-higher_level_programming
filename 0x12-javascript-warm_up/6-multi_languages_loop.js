76bc84e6de2db89305fd946342ed7331b8678476
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-01 15:55:22 -0300

    d

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,8 @@
 // Write a script that prints 3 lines
 
 let i = 0;
-let array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
 while (array[i]) {
   console.log(array[i]);
   i++;
