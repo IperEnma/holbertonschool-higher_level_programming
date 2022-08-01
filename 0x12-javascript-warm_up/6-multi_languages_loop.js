98c53d32284ca1b9692c337c6a09b604ef52ab52
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-01 15:08:33 -0300

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,8 +3,6 @@
 
 const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-let count = 0;
-
-for (count = 0; count < array.length; count++) {
-  console.log(array[count]);
+for (let string of array) {
+  console.log(string);
 }
