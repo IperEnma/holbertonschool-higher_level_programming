13d662f813d1626e5b750f24964e99529b94c4e3
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-01 16:33:48 -0300

    prints x times “C is fun”

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// script that prints x times "C is fun"
+
+let integer = parseInt(process.argv[2]);
+
+if (Number.isInteger(integer)) {
+  while (integer !== 0) {
+    console.log('C is fun');
+	  integer--;
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
