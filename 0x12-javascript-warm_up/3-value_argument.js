e4e3c67a9489908831a9f1382aeca75c28fd2c98
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-01 13:34:39 -0300

    the first argument passed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,10 @@
 // prints the first argument
 
 import { argv } from 'node:process';
-if (argv.length > 2) {
+let count = 0;
+for (count = 0; argv[count]; count++) {
+}
+if (count > 2) {
   console.log(argv[2]);
 } else {
   console.log('No argument');
