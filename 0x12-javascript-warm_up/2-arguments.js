75ecf3dfd191da61f94530585f29aa70b5174f90
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-01 13:00:07 -0300

    prints

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 import { argv } from 'node:process';
 
 // prints a message depending of args
-if (argv.length === 2) {
+if (argv.length <= 2) {
   console.log('No argument');
 } else if (argv.length === 3) {
   console.log('Argument found');
