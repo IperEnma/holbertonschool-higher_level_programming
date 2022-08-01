6839134939cc9767f7b8df26afed13e87936d5dd
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-01 16:52:17 -0300

    that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ const integer = parseInt(process.argv[2]);
 
 if (Number.isInteger(integer)) {
   for (let i = 0; i < integer; i++) {
-    console.log('x'.repeat(integer));
+    console.log('X'.repeat(integer));
   }
 } else {
   console.log('Missing size');
