2996ffb4e73d007157b6d3da5afccd7331328a7c
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-02 14:42:27 -0300

    You are not allow to use the built-in method reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,6 +2,8 @@
 // reversed version of a list
 
 exports.esrever = function (list) {
+  let i = 0;
+  let j = 0;
   for (i = 0, j = list.length - 1; i < j; i++, j--) {
     const temp = list[i];
     list[i] = list[j];
