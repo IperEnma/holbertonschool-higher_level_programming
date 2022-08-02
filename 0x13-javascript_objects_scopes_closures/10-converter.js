798249975caa5ff80f272f164b4bacb0c67b5b26
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-02 13:52:08 -0300

    any variable

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 // 10 to another base passed as argument
 
-let baseG;
-
 function myConverser (num) {
   const result = num.toString(baseG);
   return (result);
 }
 
 exports.converter = function (base) {
-  baseG = base;
-  return (myConverser);
+  return (myConverser(base));
 };
