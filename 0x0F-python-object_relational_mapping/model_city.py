99c3a257e361f2089895218b7ed4e2d8ee57ed88
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-06 18:45:57 -0300

    foreign keyt

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,7 +2,7 @@
 """ class definition of a State and an instance Base """
 
 
-from sqlalchemy import Table, Column, BigInteger, String
+from sqlalchemy import ForeignKey, Column, BigInteger, String
 from sqlalchemy import MetaData
 from sqlalchemy.dialects import mysql
 from model_state import Base
@@ -20,5 +20,9 @@ class City(Base):
             unique=True
             )
     name = Column(String(128), nullable=False)
-    state_id = Column(mysql.INTEGER(11), nullable=False)
+    state_id = Column(
+            mysql.INTEGER(11),
+            ForeignKey("state.id"),
+            nullable=False
+            )
     __table_args__ = {'mysql_charset ': ' latin1 '}
