1d3dc50ee3b274ae1d9fef4c07010b81c955cbd3
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-06 18:07:27 -0300

    only id

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
 
     if (state):
         for data in state:
-            print("{}: {}".format(data.id, data.name))
+            print("{}".format(data.id)
     else:
         print("Not found")
     session.close()
