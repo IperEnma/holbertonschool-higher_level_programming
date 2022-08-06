fae01f216db9c65a9f9954778685b663f16287a5
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-06 18:01:39 -0300

    integer

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,7 @@
 """ class definition of a State and an instance Base """
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Integer, Column, String
+from sqlalchemy import Column, String
 from sqlalchemy.dialects import mysql
 
 Base = declarative_base()
