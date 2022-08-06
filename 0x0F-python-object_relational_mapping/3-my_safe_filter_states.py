7f657d41f9898f7ea1ac28eee36b34cc610a4642
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-06 17:29:53 -0300

    control

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,12 +14,13 @@ if (__name__ == '__main__'):
             db="hbtn_0e_0_usa",
             charset="utf8"
             )
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE BINARY name\
-            =%s ORDER BY id ASC", (argv[4], ))
-    row_states = cursor.fetchall()
+    if (conn):
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM states WHERE BINARY name\
+                =%s ORDER BY id ASC", (argv[4], ))
+        row_states = cursor.fetchall()
 
-    for state in row_states:
-        print(state)
-    cursor.close()
-    conn.close()
+        for state in row_states:
+            print(state)
+        cursor.close()
+        conn.close()
