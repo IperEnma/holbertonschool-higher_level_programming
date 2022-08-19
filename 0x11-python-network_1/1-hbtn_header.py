0566cd771d6dd8158803428876ff471b4ea292ae
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 12:30:59 -0300

    error

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """sends a request to the URL and displays the value of the X-Request-Id"""
 
-import urllib
+import urllib.parse
+import urllib.request
 import sys
 
 url = sys.argv[1]
@@ -10,7 +11,7 @@ values = {'X-Request-Id': sys.argv[2]}
 data = urllib.parse.urlencode(values)
 url = url + '?' + data
 
-with urrlib.request.urlopen(url) as response:
+with urllib.request.urlopen(url) as response:
     for key, value in response.info().items():
         if key == 'X-Request-Id':
             print(value)
