cc1492fc3c7966befa5f588054476e678c3f6c9f
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 20:32:22 -0300

    asd

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,4 +17,4 @@ if __name__ == '__main__':
     elif(len(result_dict) == 0):
         print("No result")
     else:
-        print("{[]} {}".format(result_dict['id'], result_dict['name']))
+        print(result_dict)
