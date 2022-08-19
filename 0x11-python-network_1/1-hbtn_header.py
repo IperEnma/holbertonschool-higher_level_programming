f9fbb1a2ab2347621695047d315f5a2f279dc9b9
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 12:39:06 -0300

    etst

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """sends a request to the URL"""
 
-import urllib.parse
-import urllib.request
+from urlib import parse, request
 import sys
 
 url = sys.argv[1]
 values = {'X-Request-Id': sys.argv[2]}
 
-data = urllib.parse.urlencode(values)
+data = parse.urlencode(values)
 url = url + '?' + data
 
-with urllib.request.urlopen(url) as response:
+with request.urlopen(url) as response:
     for key, value in response.info().items():
         if key == 'X-Request-Id':
             print(value)
