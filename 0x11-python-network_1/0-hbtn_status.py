a67dec5cba59b9c923c30dc7937179aa2a3e8f29
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 12:57:52 -0300

    bode response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,7 @@
 import urllib.request
 
 with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+    print("Bode response:")
     print("- type: {}".format(response.code))
     html = response.read()
     print("- content: {}".format(html))
