64644778a7b9d2a7909e92e2f1d617026f6ef41b
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 13:08:04 -0300

    tab

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ import urllib.request
 with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
     print("Body response:")
     html = response.read()
-    print("{}- type: {}".format(11, type(html)))
-    print("{}- content: {}".format(11, html))
-    print("{}- utf8 content: {}".format(11, html.decode()))
+    print("    - type: {}".format(type(html)))
+    print("    - content: {}".format(html))
+    print("    - utf8 content: {}".format(html.decode()))
