008a008ac03d80d5b96511702ccae8b510300418
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 16:19:29 -0300

    decode

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,15 +2,15 @@
 """sends a request to the URL"""
 import sys
 from urllib import request, parse
-from urllib.error import URLError
+from urllib.error import HTTPError
 
 if __name__ == '__main__':
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
     data = parse.urlencode(values)
-    data = data.encode('utf-8')
+    data = data.encode()
 
-    try:
-        request.urlopen(url, data)
-    except URLError as e:
-        print(e.reason)
+    with request.urlopen(url, data) as response:
+        html = response.read()
+        print(html.decode())
+
