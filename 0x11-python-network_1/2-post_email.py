0475835b990a8cd43b53f440d5a459d4a7f0a9d7
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 16:32:47 -0300

    ptyycode

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,9 +8,8 @@ if __name__ == '__main__':
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
     data = parse.urlencode(values)
-    data = data.encode('utf-8')
+    data = data.encode()
 
     with request.urlopen(url, data) as response:
         html = response.read()
-        print(html.decode('utf-8'))
-
+        print(html.decode())
