d9bab698ec9abb8c7b1704225a8511814842e7fb
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 17:55:43 -0300

    raise error

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""sends a request to the URL and display"""
+from sys import argv
+import requests
+from requests.exceptions import HTTPError
+
+
+if __name__ == '__main__':
+    url = argv[1]
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.content.decode())
+    except HTTPError as error:
+        if(error.response.status_code >= 400)
+            print(error.response.status_code)
