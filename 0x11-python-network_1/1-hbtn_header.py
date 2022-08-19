db9981344d4342814e51367b7e4c8b3722a4c4fd
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 13:19:22 -0300

    urrlib

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """sends a request to the URL"""
 
-from urllib import request
+import urllib.request
 import sys
 
 url = sys.argv[1]
 """values = {'X-Request-Id': sys.argv[2]}
 data = parse.urlencode(values)
 url = url + '?' + data"""
-with request.urlopen(url) as response:
+with urllib.request.urlopen(url) as response:
     """for key, value in response.info().items():
         if key == 'X-Request-Id':
             print(value)"""
