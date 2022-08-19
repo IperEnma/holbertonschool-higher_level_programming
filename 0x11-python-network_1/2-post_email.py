a4b0bfb19fd288759e97eab1c088d40736c69435
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 13:38:51 -0300

    post

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""sends a request to the URL"""
+from urllib import request, parse
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+    data = parse.urlencode(values)
+    url = url + '?' + data
+    print(url)
+    with request.urlopen(url) as response:
+        if hasattr(response.info(), 'email'):
+                print(response.info().get('email'))
+
