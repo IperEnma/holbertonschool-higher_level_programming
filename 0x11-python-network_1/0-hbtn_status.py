ae34081c9ef3d944d45bb58c6e7c439376e951d1
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 12:27:15 -0300

    first check

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""scrip that fetches"""
+
+import urllib.request
+
+with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+    print("- type: {}".format(response.code))
+    html = response.read()
+    print("- content: {}".format(html))
+    print("- utf8 content: {}".format(html.decode()))
