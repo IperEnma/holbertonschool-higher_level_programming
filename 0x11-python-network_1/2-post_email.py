f69335472deeb9f5e86d5dec9c5eb29d3a6b55a4
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 15:29:33 -0300

    asd

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,7 +2,7 @@
 """sends a request to the URL"""
 import sys
 from urllib import request, parse
-from urllib.error import HTTPError
+from urllib.error import URLError
 
 if __name__ == '__main__':
     url = sys.argv[1]
@@ -10,5 +10,7 @@ if __name__ == '__main__':
     data = parse.urlencode(values)
     data = data.encode('utf-8')
 
-    with request.urlopen(url, data) as response:
-        print(f.read().decode('utf-8'))
+    try:
+        request.urlopen(url, data)
+    except URLError as e:
+        print(e.reason)
