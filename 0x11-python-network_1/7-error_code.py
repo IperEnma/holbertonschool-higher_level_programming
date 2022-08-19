604f4c265dbccb1758201259e3b90f7f940bb892
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 17:57:10 -0300

    hand errors

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,11 +7,10 @@ from requests.exceptions import HTTPError
 
 if __name__ == '__main__':
     url = argv[1]
-
     try:
         response = requests.get(url)
         response.raise_for_status()
         print(response.content.decode())
     except HTTPError as error:
-        if(error.response.status_code >= 400)
+        if(error.response.status_code >= 400):
             print(error.response.status_code)
