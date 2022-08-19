e52f027610a42397b2854907524022acfe042964
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 13:15:02 -0300

    no post request

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,11 +5,9 @@ from urllib import parse, request
 import sys
 
 url = sys.argv[1]
-values = {'X-Request-Id': sys.argv[2]}
-
-data = parse.urlencode(values)
-url = url + '?' + data
-
+#values = {'X-Request-Id': sys.argv[2]}
+#data = parse.urlencode(values)
+#url = url + '?' + data
 with request.urlopen(url) as response:
     for key, value in response.info().items():
         if key == 'X-Request-Id':
