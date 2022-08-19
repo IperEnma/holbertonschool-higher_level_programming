3cdc40e8594137a7996acacf55708bfb9995b0ca
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 14:48:04 -0300

    enconding

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,10 +7,8 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
-    data = parse.urlencode(values)
-    url = url + '?' + data
-    print(url)
-    with request.urlopen(url) as response:
+    data = parse.urlencode(values, 'utf-8')
+    full_url = url + '?' + data
+    with request.urlopen(full_url) as response:
         if hasattr(response.info(), 'email'):
-                print(response.info().get('email'))
-
+            print(response.info().get('email'))
