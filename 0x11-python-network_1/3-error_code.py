477b5d7198f7d8407f801167068361a1da91943c
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 16:49:38 -0300

    errors

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""handled errors"""
+from sys import argv
+from urllib import request
+from urllib.error import HTTPError
+
+
+if __name__ == '__main__':
+    url = argv[1]
+
+    try:
+        with request.urlopen(url) as response:
+            html = response.read()
+            print(html.decode())
+    except HTTPError as error:
+        print(error.code)
