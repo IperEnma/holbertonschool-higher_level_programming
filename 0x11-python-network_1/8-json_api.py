2ffe99ac56fa17cb72547eb16abeb30261dcb60a
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 20:31:04 -0300

    dict

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""JSON API PYTHON """
+from sys import argv
+import requests
+
+if __name__ == '__main__':
+    url = "http://0.0.0.0:5000/search_user"
+    if (len(argv) > 1):
+        data = {'q': argv[1]}
+    else:
+        data = {'q': ""}
+    response = requests.post(url, data)
+    result = response.content.decode()
+    result_dict = eval(result)
+    if(type(result_dict) != dict):
+        print("Not a valid JSON")
+    elif(len(result_dict) == 0):
+        print("No result")
+    else:
+        print("{[]} {}".format(result_dict['id'], result_dict['name']))
