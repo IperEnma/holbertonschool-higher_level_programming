3b58d3a12b66c9dddb917fa855272c16ee24f98d
IperEnma <enmanuelhernandez1843@gmail.com>
2022-08-19 12:29:05 -0300

    import fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """sends a request to the URL and displays the value of the X-Request-Id"""
 
-from urllib import request, parse
+import urllib
 import sys
 
 url = sys.argv[1]
 values = {'X-Request-Id': sys.argv[2]}
 
-data = parse.urlencode(values)
+data = urllib.parse.urlencode(values)
 url = url + '?' + data
 
-with request.urlopen(url) as response:
+with urrlib.request.urlopen(url) as response:
     for key, value in response.info().items():
         if key == 'X-Request-Id':
             print(value)
