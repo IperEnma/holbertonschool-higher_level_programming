905ff34061c807f85ee9eb2c90f2c9797bf3fb00
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-12 12:53:05 -0300

    semistandard style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const fs =  require ( 'fs' ) ;
+const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
-  if  (err)  {
+  if (err) {
     console.error(err);
     return;
   }
