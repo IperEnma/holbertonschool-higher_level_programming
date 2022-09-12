a6f5822c1024feaef7ceb556077952f3977c8472
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-12 14:05:15 -0300

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,4 +6,7 @@ const axios = require('axios').default;
 axios.get(process.argv[2])
   .then(function (response) {
     console.log('code: ' + response.status);
+  })
+  .catch(function (error) {
+    console.log('code: ' + error.response.status);
   });
