f12b170174442d1a93c462bfeaa1ce4a15f9c906
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-12 17:46:00 -0300

    films count by people

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,9 +4,15 @@
 const axios = require('axios').default;
 
 // Make a request for a user with a given ID
-axios.get('https://swapi-api.hbtn.io/api/people/18')
+axios.get(process.argv[2])
   .then(function (response) {
-    console.log(response.data.films.length);
+    let count = 0;
+    for (let idx = 0; idx < response.data.results.length; idx++) {
+	  if (Object.values(response.data.results[idx].characters).indexOf('https://swapi-api.hbtn.io/api/people/18/') > -1) {
+        count += 1;
+	  }
+    }
+    console.log(count);
   })
   .catch(function (error) {
     // handle error
