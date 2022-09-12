1755dd2922e500feca1b8f6375a51af5e2ac092c
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-12 18:31:06 -0300

    test

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,17 +4,18 @@
 const axios = require('axios').default;
 
 // Make a request for a user with a given ID
-axios.get(process.argv[2])
+const url = process.argv[2];
+axios.get(url)
   .then(function (response) {
     let count = 0;
     for (let idx = 0; idx < response.data.results.length; idx++) {
       if (Object.values(response.data.results[idx].characters).indexOf('https://swapi-api.hbtn.io/api/people/18/') > -1) {
-        count += 1;
-      }
+      	count += 1;
+	  }
     }
     console.log(count);
   })
   .catch(function (error) {
     // handle error
-    console.log(error);
+    console.log(error.response.status);
   });
